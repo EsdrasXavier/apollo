cb5d99d34757eef3a75dee06f3480fa94f3bb527
Esdras Xavier <esdrasxa@gmail.com>
2020-09-08 21:47:02 -0300

    Adicionado comandos de play, e stop.

--- src/client.js
+++ src/client.js
@@ -1,20 +1,49 @@
 const config = require("./config.json");
+const ytdl = require('ytdl-core');
+
+const PLAY = 'play';
+const STOP = 'stop';
 
 const handlers = {
-  'play': async message => {
-    if (message.member.voice.channel) {
-      const connection = await message.member.voice.channel.join();
-    } else {
-      message.reply('You need to join a voice channel first!');
+  [PLAY]: async (message, args) => {
+    if (message.channel.type === 'dm') return;
+
+    const voiceChannel = message.member.voice.channel;
+
+    if (!voiceChannel) {
+      return message.reply('You need to join a voice channel first!');
     }
+
+    const youtubeUrl = args[0];
+
+    if (youtubeUrl === '') {
+      return message.reply('You must provide an youtube link');
+    }
+
+    voiceChannel.join().then(connection => {
+      const stream = ytdl(youtubeUrl, { filter: 'audioonly' });
+      const dispatcher = connection.play(stream);
+
+      // dispatcher.on('finish', () => voiceChannel.leave());
+    });
+  },
+  [STOP]: message => {
+    const voiceChannel = message.member.voice.channel;
+    if (!voiceChannel) {
+      return message.reply('You need to join a voice channel first!');
+    }
+
+    voiceChannel.leave();
   }
 }
 
 const messageHandler = msg => {
-  const { content } = msg;
-  if (content.startsWith(config.prefix)) {
-    handlers[content.substring(1)](msg);
-  }
+  if (!msg.content.startsWith(config.prefix) || msg.author.bot) return;
+  if (msg.channel.type === 'dm') return;
+
+  const args = msg.content.slice(config.prefix).trim().split(' ');
+  const command = args.shift().toLowerCase();
+  handlers[command.substring(1)](msg, args);
 }
 
 module.exports = {
